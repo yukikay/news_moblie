fdb2dbd8f95fedec6e1341d1b8055f99ffdc9c6a
yukikay <yukixkw@gmail.com>
2022-09-19 22:45:09 +0800

    create a new project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: yukikay yukixkw@gmail.com
+ * @Date: 2022-09-19 21:35:20
+ * @LastEditors: yukikay yukixkw@gmail.com
+ * @LastEditTime: 2022-09-19 22:05:06
+ * @FilePath: \hmtt\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
